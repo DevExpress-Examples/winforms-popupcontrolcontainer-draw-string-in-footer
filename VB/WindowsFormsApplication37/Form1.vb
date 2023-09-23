2da93b36c8ddda4c653ecabc01cf57e9a2d5242e
DevExpressExampleBot <fakeEmail>
2023-09-23 21:27:44 +0400

    Source auto update [skip ci]

--- VB/WindowsFormsApplication37/Form1.vb
+++ VB/WindowsFormsApplication37/Form1.vb
@@ -1,17 +1,13 @@
-﻿Imports System
-Imports System.Collections.Generic
+Imports System
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
 Imports System.Windows.Forms
-Imports DevExpress.Utils.Win
 Imports DevExpress.XtraBars
 Imports DevExpress.XtraBars.Controls
 
 Namespace WindowsFormsApplication37
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
         Public Sub New()
@@ -21,13 +17,13 @@ Namespace WindowsFormsApplication37
 
         Private Sub popupControlContainer1_Popup(ByVal sender As Object, ByVal e As EventArgs)
             Dim control As PopupControlContainer = TryCast(sender, PopupControlContainer)
-            Dim barControl As PopupContainerBarControl = (TryCast(control.Parent, PopupContainerBarControl))
+            Dim barControl As PopupContainerBarControl = TryCast(control.Parent, PopupContainerBarControl)
             AddHandler barControl.Paint, AddressOf Form1_Paint
         End Sub
 
         Private Sub Form1_Paint(ByVal sender As Object, ByVal e As PaintEventArgs)
-            Dim barControl As PopupContainerBarControl = (TryCast(sender, PopupContainerBarControl))
-            Dim offsetLocation As New Point(barControl.CloseButton.Location.X + barControl.CloseButton.Width + 3, barControl.CloseButton.Location.Y)
+            Dim barControl As PopupContainerBarControl = TryCast(sender, PopupContainerBarControl)
+            Dim offsetLocation As Point = New Point(barControl.CloseButton.Location.X + barControl.CloseButton.Width + 3, barControl.CloseButton.Location.Y)
             e.Graphics.DrawString("Custom string", barControl.Font, Brushes.Green, offsetLocation)
         End Sub
     End Class
